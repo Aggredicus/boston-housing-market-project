87863fb78f9b8ebe8308dd5726317cddc8c81092
Aggredicus <dillonmichaellee@gmail.com>
2016-02-25 20:38:19 -0500

    Add the rest of the structure from the iPyNotebook

--- boston-housing-market-prediction.py
+++ boston-housing-market-prediction.py
@@ -27,3 +27,137 @@ feature_labels = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD
 for label in feature_labels:
     feature_position = feature_labels.index('%s' % label)
     label = housing_features[:, feature_position]
+
+# Put any import statements you need for this code block here
+from sklearn import cross_validation
+
+
+def shuffle_split_data(X, y):
+    """ Shuffles and splits data into 70% training and 30% testing subsets,
+        then returns the training and testing subsets. """
+
+    # Shuffle and split the data n_iter times
+    rs = cross_validation.ShuffleSplit(len(y), n_iter=1, test_size=0.3, random_state=0)
+
+    for train_index, test_index in rs:
+        X_train = X[train_index]
+        y_train = y[train_index]
+        X_test = X[test_index]
+        y_test = y[test_index]
+
+    # Return the training and testing data subsets
+    return X_train, y_train, X_test, y_test
+
+# Put any import statements you need for this code block here
+from sklearn.metrics import mean_squared_error
+
+def performance_metric(y_true, y_predict):
+    """ Calculates and returns the total error between true and predicted values
+        based on a performance metric chosen by the student. """
+
+    error = mean_squared_error(y_true, y_predict)
+    return error
+
+# Put any import statements you need for this code block
+
+def fit_model(X, y):
+    """ Tunes a decision tree regressor model using GridSearchCV on the input data X
+        and target labels y and returns this optimal model. """
+
+    # Create a decision tree regressor object
+    regressor = DecisionTreeRegressor()
+
+    # Set up the parameters we wish to tune
+    parameters = {'max_depth':(1,2,3,4,5,6,7,8,9,10)}
+
+    # Make an appropriate scoring function
+    scoring_function = None
+
+    # Make the GridSearchCV object
+    reg = None
+
+    # Fit the learner to the data to obtain the optimal model with tuned parameters
+    reg.fit(X, y)
+
+    # Return the optimal model
+    return reg
+
+def learning_curves(X_train, y_train, X_test, y_test):
+    """ Calculates the performance of several models with varying sizes of training data.
+        The learning and testing error rates for each model are then plotted. """
+
+    print "Creating learning curve graphs for max_depths of 1, 3, 6, and 10. . ."
+
+    # Create the figure window
+    fig = pl.figure(figsize=(10,8))
+
+    # We will vary the training set size so that we have 50 different sizes
+    sizes = np.round(np.linspace(1, len(X_train), 50))
+    train_err = np.zeros(len(sizes))
+    test_err = np.zeros(len(sizes))
+
+    # Create four different models based on max_depth
+    for k, depth in enumerate([1,3,6,10]):
+
+        for i, s in enumerate(sizes):
+
+            # Setup a decision tree regressor so that it learns a tree with max_depth = depth
+            regressor = DecisionTreeRegressor(max_depth = depth)
+
+            # Fit the learner to the training data
+            regressor.fit(X_train[:s], y_train[:s])
+
+            # Find the performance on the training set
+            train_err[i] = performance_metric(y_train[:s], regressor.predict(X_train[:s]))
+
+            # Find the performance on the testing set
+            test_err[i] = performance_metric(y_test, regressor.predict(X_test))
+
+        # Subplot the learning curve graph
+        ax = fig.add_subplot(2, 2, k+1)
+        ax.plot(sizes, test_err, lw = 2, label = 'Testing Error')
+        ax.plot(sizes, train_err, lw = 2, label = 'Training Error')
+        ax.legend()
+        ax.set_title('max_depth = %s'%(depth))
+        ax.set_xlabel('Number of Data Points in Training Set')
+        ax.set_ylabel('Total Error')
+        ax.set_xlim([0, len(X_train)])
+
+    # Visual aesthetics
+    fig.suptitle('Decision Tree Regressor Learning Performances', fontsize=18, y=1.03)
+    fig.tight_layout()
+    fig.show()
+
+def model_complexity(X_train, y_train, X_test, y_test):
+    """ Calculates the performance of the model as model complexity increases.
+        The learning and testing errors rates are then plotted. """
+
+    print "Creating a model complexity graph. . . "
+
+    # We will vary the max_depth of a decision tree model from 1 to 14
+    max_depth = np.arange(1, 14)
+    train_err = np.zeros(len(max_depth))
+    test_err = np.zeros(len(max_depth))
+
+    for i, d in enumerate(max_depth):
+        # Setup a Decision Tree Regressor so that it learns a tree with depth d
+        regressor = DecisionTreeRegressor(max_depth = d)
+
+        # Fit the learner to the training data
+        regressor.fit(X_train, y_train)
+
+        # Find the performance on the training set
+        train_err[i] = performance_metric(y_train, regressor.predict(X_train))
+
+        # Find the performance on the testing set
+        test_err[i] = performance_metric(y_test, regressor.predict(X_test))
+
+    # Plot the model complexity graph
+    pl.figure(figsize=(7, 5))
+    pl.title('Decision Tree Regressor Complexity Performance')
+    pl.plot(max_depth, test_err, lw=2, label = 'Testing Error')
+    pl.plot(max_depth, train_err, lw=2, label = 'Training Error')
+    pl.legend()
+    pl.xlabel('Maximum Depth')
+    pl.ylabel('Total Error')
+    pl.show()
